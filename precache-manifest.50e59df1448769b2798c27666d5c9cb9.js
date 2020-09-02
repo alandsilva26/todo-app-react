4555a05abfbcb3bbf7791c8f29aa042d6185bc37
Alan Dsilva <deathbringer2604@gmail.com>
2020-09-03 03:28:50 +0530

    Updates

---
+++ precache-manifest.50e59df1448769b2798c27666d5c9cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3a72788ff4ac11721848539df4695b",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "2f76bdaeb8106a0e1685",
+    "url": "/todo-app-react/static/css/main.f50211f0.chunk.css"
+  },
+  {
+    "revision": "b8927a73b90143768b2e",
+    "url": "/todo-app-react/static/js/2.cfebbbcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.cfebbbcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f76bdaeb8106a0e1685",
+    "url": "/todo-app-react/static/js/main.2f8d59f0.chunk.js"
+  },
+  {
+    "revision": "85cae369ffabab9cc3e3",
+    "url": "/todo-app-react/static/js/runtime-main.e0d73842.js"
+  }
+]);
